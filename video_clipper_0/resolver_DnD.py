4b4e7a5c7972fee3d3fa69bf161980b639508d82
HK <>
2021-01-26 20:37:23 +0900

    ダヴィンチリゾルブのやつを csv ファイルを読み込むようにしました。

--- video_clipper_0/resolver_DnD.py
+++ video_clipper_0/resolver_DnD.py
@@ -1,40 +1,46 @@
+import csv
+import os
+import sys
 
-video_path = ""
-export_path = ""
-clip_time = [[203.74,213.76],
-[244.94,255.6],
-[455.92,488.1],
-[512.12,546.82],
-[836.56,843.84],
-[1278.24,1297.94],
-[1317.14,1355.34],
-[1454.98,1480.0],
-[1548.04,1555.36],
-[1565.64,1570.74],
-[1609.16,1621.7],
-[4051.44,4059.7],
-[5207.54,5237.44],
-[6469.2,6482.94],
-[6727.26,6740.82],
-[7965.84,7988.0],
-[10038.52,10054.64]]
+dir_path = ""
+csv_filename = "test" + ".csv"
 
+print("LOAD CSV")
+csv_path = os.path.join(dir_path, csv_filename)
+if os.path.isfile(csv_path):
+    url = None
+    clip_times = []
+    with open(csv_path, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        for line in reader:
+            if url is None:
+                if len(line) > 0:
+                    url = line[0]
+            else:
+                if len(line) > 1:
+                    clip_times.append((float(line[0]), float(line[1])))
+else:
+    print("ERROR:", csv_path, "does not exist.")
+    sys.exit()
 
+video_id = url.split("=")[-1].strip()
 
-print("START")
+video_path = os.path.join(dir_path, video_id + ".mp4")
+if not os.path.isfile(video_path):
+    print("ERROR:", csv_path, "does not exist.")
+    sys.exit()
+
+export_path = dir_path
 
 print("CREATE PROJECT")
-
 projectManager = resolve.GetProjectManager()
 project = projectManager.CreateProject("TEST")
 
 print("MEDIA")
-
 mediapool = project.GetMediaPool()
 rootFolder = mediapool.GetRootFolder()
 
 print("LOAD VIDEO")
-
 video1 = resolve.GetMediaStorage().AddItemsToMediaPool(video_path)
 
 video2 = rootFolder.GetClipList()[0]
@@ -54,22 +60,19 @@ project.SetSetting("timelineResolutionWidth", video_size_str[0])
 project.SetSetting("timelineResolutionHeight", video_size_str[1])
 
 print("TIMELINE")
-
 timeline = mediapool.CreateEmptyTimeline("Timeline 1")
 
 print("ADD VIDEO")
-
-for clip_t in clip_time:
-    print("frame", int(clip_t[0] * video_fps), int(clip_t[1] * video_fps))
+for clip in clip_times:
+    print("frame", int(clip[0] * video_fps), int(clip[1] * video_fps))
     clip_part = {
         "mediaPoolItem": video2,
-        "startFrame": int(clip_t[0] * video_fps),
-        "endFrame": int(clip_t[1] * video_fps),
+        "startFrame": int(clip[0] * video_fps),
+        "endFrame": int(clip[1] * video_fps),
     }
     mediapool.AppendToTimeline([clip_part])
 
 print("SAVE")
-
 #projectManager.SaveProject()
 a = projectManager.ExportProject("EXPORT_TEST", export_path)
 print(a)
